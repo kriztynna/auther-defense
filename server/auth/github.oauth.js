6583d9da0497225b2364a8f07c09779fe0d4b193
Karen MacPherson <kam0112@gmail.com>
2015-09-03 15:50:44 -0400

    initial

---
+++ server/auth/github.oauth.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var githubSecrets = require('./secrets').githubSecrets;
+
+var router = require('express').Router();
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('github'));
+
+router.get('/callback', passport.authenticate('github', {
+	successRedirect: '/stories',
+	failureRedirect: '/signup'
+}));
+
+passport.use(new GitHubStrategy({
+	clientID: githubSecrets.clientID,
+	clientSecret: githubSecrets.clientSecret,
+	callbackURL: 'http://127.0.0.1:8080/auth/github/callback'
+}, function (token, refreshToken, profile, done) { 
+	User.findOne({'github.id': profile.id }, function (err, user) {
+		if (err) done(err);
+		else if (user) done(null, user);
+		else {
+			var email = (profile.emails) 
+				? email = profile.emails[0].value 
+				: [profile.username , 'fake-auther-email.com'].join('@');
+
+			User.create({
+				email: email,
+				name: profile.displayName,
+				github: {
+					id: profile.id,
+					name: profile.displayName,
+					email: email,
+					token: token
+				}
+			}, done);
+		}
+	});
+}));
+
+module.exports = router;
---
+++ server/auth/google.oauth.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var googleSecrets = require('./secrets').googleSecrets;
+
+
+var router = require('express').Router();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('google', {
+	scope: 'email'
+}));
+
+router.get('/callback', passport.authenticate('google', {
+	successRedirect: '/stories',
+	failureRedirect: '/signup'
+}));
+
+passport.use(new GoogleStrategy({
+	clientID: googleSecrets.clientID,
+	clientSecret: googleSecrets.clientSecret,
+	callbackURL: 'http://127.0.0.1:8080/auth/google/callback'
+}, function (token, refreshToken, profile, done) { 
+	User.findOne({'google.id': profile.id }, function (err, user) {
+		if (err) done(err);
+		else if (user) done(null, user);
+		else {
+			var email = profile.emails[0].value;
+			User.create({
+				email: email,
+				photo: profile.photos[0].value,
+				name: profile.displayName,
+				google: {
+					id: profile.id,
+					name: profile.displayName,
+					email: email,
+					token: token
+				}
+			}, done);
+		}
+	});
+}));
+
+module.exports = router;
