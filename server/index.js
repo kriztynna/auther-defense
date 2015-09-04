242983f020fa46e631c2cda7fd8c67d37ca57c3a
Karen MacPherson <kam0112@gmail.com>
2015-09-04 11:25:11 -0400

    changed server to https

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 'use strict';
 
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync(__dirname+'/key.pem', 'utf8'),
+  cert: fs.readFileSync(__dirname+'/cert.pem', 'utf8')
+};
+
 var app = require('./app'),
-	db = require('./db');
+    db = require('./db');
 
 var port = 8080;
-var server = app.listen(port, function () {
-	console.log('HTTP server patiently listening on port', port);
-});
+var httpsServer = https.createServer(options, app)
+  .listen(port, function() {
+    console.log('HTTPS server listening on port', port);
+  });
 
-module.exports = server;
+
+module.exports = httpsServer;
